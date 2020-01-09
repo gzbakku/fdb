1916b93359cb562dde22fcc8d431853ce84f3cc7
gzbakku <gzbakku@gmail.com>
2020-01-09 15:31:06 +0530

    pre publiush cert to cargo

--- cert/src/lib.rs
+++ cert/src/lib.rs
@@ -13,35 +13,42 @@ pub mod builder;
 
 use builder::Builder;
 
-fn test(){
+#[cfg(test)]
+mod test {
+    use crate;
 
-    let mut build = Builder::new();
+    #[test]
+    fn test(){
 
-    build.set_key_path("D://workstation/expo/rust/fdb/cert/keys/key.pem".to_string());
-    build.set_certificate_path("D://workstation/expo/rust/fdb/cert/keys/cert.pem".to_string());
-    build.set_key_size(4048);
+        let mut build = Builder::new();
 
-    build.issuer.set_country("IN".to_string());
-    build.issuer.set_state("UP".to_string());
-    build.issuer.set_location("GZB".to_string());
-    build.issuer.set_org("DAACHI".to_string());
-    build.issuer.set_common_name("https://daachi.in".to_string());
+        build.set_key_path("D://workstation/expo/rust/fdb/cert/keys/key.pem".to_string());
+        build.set_certificate_path("D://workstation/expo/rust/fdb/cert/keys/cert.pem".to_string());
+        build.set_key_size(4048);
 
-    build.subject.set_country("IN".to_string());
-    build.subject.set_state("UP".to_string());
-    build.subject.set_location("GZB".to_string());
-    build.subject.set_org("DAACHI".to_string());
-    build.subject.set_common_name("127.0.0.1".to_string());
+        build.issuer.set_country("IN".to_string());
+        build.issuer.set_state("UP".to_string());
+        build.issuer.set_location("GZB".to_string());
+        build.issuer.set_org("DAACHI".to_string());
+        build.issuer.set_common_name("https://daachi.in".to_string());
 
-    //println!("build : {:?}",build);
+        build.subject.set_country("IN".to_string());
+        build.subject.set_state("UP".to_string());
+        build.subject.set_location("GZB".to_string());
+        build.subject.set_org("DAACHI".to_string());
+        build.subject.set_common_name("127.0.0.1".to_string());
 
-    match create(&mut build) {
-        Ok(_)=>{
-            common::log("ssl files created successfully");
-        },
-        Err(_)=>{
-            common::error("failed to create ssl files");
+        //println!("build : {:?}",build);
+
+        match create(&mut build) {
+            Ok(_)=>{
+                common::log("ssl files created successfully");
+            },
+            Err(_)=>{
+                common::error("failed to create ssl files");
+            }
         }
+
     }
 
 }
